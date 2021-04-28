d10375cf534691015d479d6604a63bd178612c1d
LUIS CARRILLO VILLARREAL <luis.carrillo.villarreal@est.una.ac.cr>
2021-04-28 01:18:55 -0600

    Agregar archivos de proyecto.

---
+++ Models/DBReservandoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Reservando.Models
+{
+    public partial class DBReservandoContext: DbContext
+    {
+        public DBReservandoContext()
+        {
+
+        }
+        public DBReservandoContext(DbContextOptions<DBReservandoContext> options) : base(options)
+        {
+
+        }
+    }
+}
